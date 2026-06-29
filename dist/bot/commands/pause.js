094481951f416112ee6e191867169449baab5ef2
weviu <weviu@yahoo.com>
2026-06-29 17:13:35 +0000

    stop tracking dist

--- dist/bot/commands/pause.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pauseCmd = pauseCmd;
-const state_1 = require("../../state");
-async function pauseCmd(ctx) {
-    state_1.state.paused = true;
-    await ctx.reply("Trading paused. Use /resume to enable.");
-}
-//# sourceMappingURL=pause.js.map
